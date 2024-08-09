e9b73ed22b07ebd75affac52c095626d531df6b3
议程 <yichengzhao5@gmail.com>
2024-08-09 21:16:34 +0800

    添加全局加载

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,11 @@ const request = axios.create({
 // 添加请求拦截器
 request.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    Toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0,
+    });
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -24,6 +29,8 @@ request.interceptors.response.use(function (response) {
     if (res.status !== 200) {
         Toast(res.message)
         return Promise.reject(res.message);
+    }else {
+        Toast.clear()
     }
     return res;
 }, function (error) {
