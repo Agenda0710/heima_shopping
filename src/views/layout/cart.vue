7cbe7780325698258ab80d9f0f36ee37b56b470c
议程 <yichengzhao5@gmail.com>
2024-08-13 00:23:15 +0800

    购物车结算

--- src/views/layout/cart.vue
+++ src/views/layout/cart.vue
@@ -43,7 +43,7 @@
             <span>合计：</span>
             <span>¥ <i class="totalPrice">{{ selPrice }}</i></span>
           </div>
-          <div v-if="!isEdit" class="goPay" :class="{disabled:selCount===0}" @click="$router.push('/pay')">结算({{ selCount }})</div>
+          <div v-if="!isEdit" @click="goPay()" class="goPay" :class="{disabled:selCount===0}">结算({{ selCount }})</div>
           <div v-else class="delete" :class="{disabled:selCount===0}" @click="handleDel()">删除</div>
         </div>
       </div>
@@ -68,7 +68,7 @@ export default {
   components: {CountBox},
   data() {
     return {
-      isEdit: false
+      isEdit: false,
     }
   },
   watch: {
@@ -102,6 +102,17 @@ export default {
         return
       }
       this.$store.dispatch('cart/delSelect', this.selCartList)
+    },
+    goPay() {
+      if (this.selCount > 0) {
+        this.$router.push({
+          path: '/pay',
+          query: {
+            mode: 'cart',
+            cartIds: this.selCartList.map(item => item.id).join(',')
+          }
+        })
+      }
     }
   }
 }
